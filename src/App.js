6b2ffed11d88bca26cefbc3a9a6f3dbb9d6b5b56
Ashraful Alam <ashrafcse3@gmail.com>
2022-12-25 10:19:50 +0000

    example-1, state & action as primitive value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { useReducer } from 'react';
 import './App.css';
 
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment':
+      return state + 1;
+    case 'decrement':
+      return state - 1;
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
   return (
     <div className="App">
-      from react js
+      <div>Count - {count}</div>
+      <button type='button' onClick={() => dispatch('increment')}>Increment</button>
+      <button type='button' onClick={() => dispatch('decrement')}>Decrement</button>
     </div>
   );
 }
